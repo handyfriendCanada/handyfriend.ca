6c5da15193e301d092a4f4f64ce1833e9f25836f
handyfriend <handyfriend.ca@gmail.com>
2024-07-22 14:12:39 -0700

    Update send-messages.ts

--- src/app/api/bot/send-messages.ts
+++ src/app/api/bot/send-messages.ts
@@ -4,14 +4,19 @@ import axios from 'axios';
 // @ts-ignore
 export default async function handler(req, res) {
   const token = process.env.TELEGRAM_BOT_TOKEN
+  const chat_id = process.env.TELEGRAM_CHAT_ID
   
   if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
+  if (!chat_id) throw new Error('TELEGRAM_CHAT_ID environment variable not found.')
   if (req.method === 'POST') {
     const { message } = req.body;
     
     try {
-      const response = await axios.post(`https://api.telegram.org/bot<${token}>/sendMessage`, {
-        text: message,
+      const response = await axios.get(`https://api.telegram.org/bot${token}/sendMessage`, {
+        params: {
+          text: message,
+          chat_id,
+        }
       });
 
       res.status(200).json({ status: 'Message sent successfully!' });
