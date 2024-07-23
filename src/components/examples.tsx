a71476b968c945e42a96967a53322fa09ede4eb6
handyfriend <handyfriend.ca@gmail.com>
2024-07-23 13:17:37 -0700

    finish

--- src/components/examples.tsx
+++ src/components/examples.tsx
@@ -5,11 +5,16 @@ import Image, { StaticImageData } from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 
-import example1 from '@/assets/example1.jpg';
-import example2 from '@/assets/example2.jpg';
-import example3 from '@/assets/example3.jpg';
-import example4 from '@/assets/example4.jpg';
-import example5 from '@/assets/example5.jpg';
+import example1 from '@/assets/example1.png';
+import example2 from '@/assets/example2.png';
+import example3 from '@/assets/example3.png';
+import example4 from '@/assets/example4.png';
+import example5 from '@/assets/example5.png';
+import example6 from '@/assets/example6.png';
+import example7 from '@/assets/example7.png';
+import example8 from '@/assets/example8.png';
+import example9 from '@/assets/example9.png';
+import example10 from '@/assets/example10.png';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -63,25 +68,34 @@ const Examples = () => {
             }}
             modules={[Pagination]}>
             <SwiperSlide>
-              <ExampleCard title='Flooring Installation' image={example1} />
+              <ExampleCard title='Plumbing' image={example9} />
             </SwiperSlide>
             <SwiperSlide>
-              <ExampleCard title='Flooring Installation' image={example2} />
+              <ExampleCard title='Installing a hanger' image={example1} />
             </SwiperSlide>
             <SwiperSlide>
-              <ExampleCard title='Flooring Installation' image={example3} />
+              <ExampleCard title='Fence extension' image={example3} />
             </SwiperSlide>
             <SwiperSlide>
-              <ExampleCard title='Flooring Installation' image={example4} />
+              <ExampleCard title='Wall demolition' image={example2} />
             </SwiperSlide>
             <SwiperSlide>
-              <ExampleCard title='Flooring Installation' image={example5} />
+              <ExampleCard title='Installation 3-way switch' image={example4} />
             </SwiperSlide>
             <SwiperSlide>
-              <ExampleCard title='Flooring Installation' image={example2} />
+              <ExampleCard title='Shelf installation' image={example6} />
             </SwiperSlide>
             <SwiperSlide>
-              <ExampleCard title='Flooring Installation' image={example3} />
+              <ExampleCard title='Deck repair' image={example5} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExampleCard title='Tv wall mounting' image={example7} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExampleCard title='Fence repair' image={example8} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <ExampleCard title='Installation of an external lamp' image={example10} />
             </SwiperSlide>
           </Swiper>
         </div>
--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -1,8 +1,12 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { Accordion, AccordionHeader, AccordionBody } from '@material-tailwind/react';
-import clsx from 'clsx';
+import React from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import clsx from "clsx";
 
 const AccordionItem = ({ title, children, isOpen, onClick }: any) => {
   return (
@@ -11,31 +15,34 @@ const AccordionItem = ({ title, children, isOpen, onClick }: any) => {
       icon={
         <svg
           className={clsx(
-            'h-[14px] w-[14px] sm:h-[18px] sm:w-[18px] transition-all',
-            isOpen && 'rotate-45',
+            "h-[14px] w-[14px] sm:h-[18px] sm:w-[18px] transition-all",
+            isOpen && "rotate-45"
           )}
-          width='15'
-          height='15'
-          viewBox='0 0 19 20'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'>
+          width="15"
+          height="15"
+          viewBox="0 0 19 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
-            d='M18.476 11.912H11.276V19.184H7.28V11.912H0.116V8.132H7.28V0.823998H11.276V8.132H18.476V11.912Z'
-            fill='#FFD705'
+            d="M18.476 11.912H11.276V19.184H7.28V11.912H0.116V8.132H7.28V0.823998H11.276V8.132H18.476V11.912Z"
+            fill="#FFD705"
           />
         </svg>
       }
       open={isOpen}
-      className='mb-2 rounded-3xl overflow-hidden bg-white border border-[#151515] px-10 sm:px-3'>
+      className="mb-2 rounded-3xl overflow-hidden bg-white border border-[#151515] px-10 sm:px-3"
+    >
       {/* @ts-ignore */}
       <AccordionHeader
         onClick={onClick}
         className={`text-black opacity-100 text-[14px] sm:text-xl font-medium border-b-0 transition-all ${
-          isOpen ? 'text-main hover:!text-main' : ''
-        }`}>
+          isOpen ? "text-main hover:!text-main" : ""
+        }`}
+      >
         {title}
       </AccordionHeader>
-      <AccordionBody className='pt-0 text-[14px] sm:text-base font-normal'>
+      <AccordionBody className="pt-0 text-[14px] sm:text-base font-normal">
         {children}
       </AccordionBody>
     </Accordion>
@@ -48,96 +55,147 @@ const Questions = () => {
   const handleOpen = (value: any) => setOpen(open === value ? 0 : value);
 
   return (
-    <div className='px-5 py-10 sm:pt-[150px] sm:pb-[170px] bg-white flex justify-center'>
-      <div className='max-w-screen-container flex flex-col gap-8 sm:gap-14'>
+    <div className="px-5 py-10 sm:pt-[150px] sm:pb-[170px] bg-white flex justify-center">
+      <div className="max-w-screen-container flex flex-col gap-8 sm:gap-14">
         <h3
-          id='questions'
-          className='scroll-m-[100px] xl:scroll-m-[200px] text-center text-black text-[32px] sm:text-5xl font-semibold leading-[30px] xs:leading-[67px]'>
-          Have any quetions?
+          id="questions"
+          className="scroll-m-[100px] xl:scroll-m-[200px] text-center text-black text-[32px] sm:text-5xl font-semibold leading-normal xs:leading-[67px]"
+        >
+          Frequently Asked Questions
+          <p className="text-[16px] lg:text-[22px] md:text-[18px] mx-auto font-light leading-normal">
+            If the FAQ below does not answer your questions, feel free to get in
+            touch or email us at handyfriend@.ca.
+          </p>
         </h3>
-        <div className='flex flex-col md:flex-row items-start justify-center gap-1 sm:gap-6'>
-          <div className='flex-1 flex flex-col gap-1 sm:gap-4'>
+        <div className="flex flex-col md:flex-row items-start justify-center gap-1 sm:gap-6">
+          <div className="flex-1 flex flex-col gap-1 sm:gap-4">
             <AccordionItem
-              title='What types of services do you offer?'
+              title="Can you do small tasks?"
               isOpen={open === 1}
-              onClick={() => handleOpen(1)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure. Lorem ipsum
-              dolor sit amet consectetur adipisicing elit.
+              onClick={() => handleOpen(1)}
+            >
+              No job is too small for us – whether it&apos;s changing a
+              hard-to-reach lightbulb, fixing a door knob, or working through a
+              to-do list around your space.
             </AccordionItem>
             <AccordionItem
-              title='How much experience do you have in this field?'
+              title="What kind of projects can you do?"
               isOpen={open === 2}
-              onClick={() => handleOpen(2)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure.
+              onClick={() => handleOpen(2)}
+            >
+              Our work scope is broad. However, we do not handle rewiring
+              electrical systems, extensive plumbing, or structural repairs.
+              <br />
+              We specialize in smaller renovation projects like installations,
+              repairs, mounting, tiling, painting, and installing drywall and
+              storage systems. We also offer some landscaping services.
             </AccordionItem>
             <AccordionItem
-              title='Are you licensed and insured?'
+              title="Do you offer services in my area?"
               isOpen={open === 5}
-              onClick={() => handleOpen(5)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure.
+              onClick={() => handleOpen(5)}
+            >
+              We provide services in the following areas of Toronto:
+              <br />• Toronto & East York
+              <br />• North York
+              <br />• Scarborough
+              <br />• Markham
+              <br />• Richmond Hill
+              <br />• Vaughan
+              <br />• Etobicoke
+              <br />• Mississauga
+              <br />• Brampton
             </AccordionItem>
             <AccordionItem
-              title='Can you provide references or examples of previous work?'
+              title="Do you work in the evenings, weekends, or holidays?"
               isOpen={open === 7}
-              onClick={() => handleOpen(7)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure.
+              onClick={() => handleOpen(7)}
+            >
+              Yes, we operate 24/7, 365 days a year. For services outside our
+              regular business hours (8 AM - 6 PM, Mon-Fri) and on some
+              holidays, we charge an additional fee.
             </AccordionItem>
             <AccordionItem
-              title='How do you determine the cost of a job?'
+              title="Do I need to provide any materials?"
               isOpen={open === 9}
-              onClick={() => handleOpen(9)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure.
+              onClick={() => handleOpen(9)}
+            >
+              While we provide all necessary materials and supplies, we
+              recommend that you purchase specific items like faucets, light
+              fixtures, or curtain rods to ensure you get exactly what you want.
+              Please consult with our specialist before purchasing to ensure
+              compatibility.
+            </AccordionItem>
+            <AccordionItem
+              title="Are your handymen trained?"
+              isOpen={open === 11}
+              onClick={() => handleOpen(11)}
+            >
+              We only hire specialists with 5+ years of experience. All new hires undergo a comprehensive training program to ensure they can tackle any challenge. Additionally, they receive regular training every 3-6 months to upgrade their skills.
             </AccordionItem>
           </div>
-          <div className='flex-1 flex flex-col gap-1 sm:gap-4'>
+          <div className="flex-1 flex flex-col gap-1 sm:gap-4">
             <AccordionItem
-              title='What is your availability, and how soon can you start a project?'
+              title="Do your handymen bring all the tools for the job?"
               isOpen={open === 4}
-              onClick={() => handleOpen(4)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure.
+              onClick={() => handleOpen(4)}
+            >
+              Absolutely! Our handymen come fully equipped with all the
+              necessary tools to tackle any repair or installation you need.
             </AccordionItem>
             <AccordionItem
-              title='Do you supply your own tools and materials?'
+              title="Are you insured?"
               isOpen={open === 3}
-              onClick={() => handleOpen(3)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure. Lorem
-              ipsum, dolor sit amet consectetur adipisicing elit. Consequatur sapiente autem
-              doloremque ratione odit non ullam sint alias eaque? Earum voluptatem magni fugiat ab
-              iste consectetur natus reiciendis nostrum quisquam explicabo accusamus, harum odit
-              illo incidunt odio quasi sequi totam ducimus pariatur? Delectus quaerat culpa, quod
-              qui fugiat perferendis rem voluptatum impedit sit ducimus distinctio nobis iste vel
-              consequuntur totam id hic libero! Odit ipsam excepturi veniam? Ex, ipsam architecto
-              adipisci fuga unde excepturi pariatur natus sit facilis eum hic nostrum veritatis!
-              Iure facilis error possimus pariatur quas, cum ullam hic incidunt assumenda quos aut
-              impedit porro repudiandae velit rerum.
+              onClick={() => handleOpen(3)}
+            >
+              Yes, we carry $2 million in general liability insurance.
             </AccordionItem>
             <AccordionItem
-              title='How long will it take to complete my project?'
+              title="What are your rates?"
               isOpen={open === 6}
-              onClick={() => handleOpen(6)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure.
+              onClick={() => handleOpen(6)}
+            >
+              • Hourly rate: $99 for the first hour + $49 for each additional
+              hour
+              <br />• Project-based pricing: Get an online quote or an in-house
+              estimate.
             </AccordionItem>
             <AccordionItem
-              title='Do you offer any guarantees or warranties on your work?'
+              title="How many handymen will come?"
               isOpen={open === 8}
-              onClick={() => handleOpen(8)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure. Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Soluta nulla accusamus vitae quas
-              quisquam neque deleniti consectetur nihil rerum eveniet, quos consequuntur quae,
-              magnam eaque nobis minima perspiciatis facilis iure aliquam mollitia suscipit ipsum
-              expedita officiis? Tempora illum quam reiciendis et? Quis doloremque possimus aut unde
-              harum, laborum optio cumque accusantium dignissimos nulla itaque ad architecto
-              temporibus quibusdam facere facilis aspernatur necessitatibus ipsam! Quas, cum eius!
-              Facere porro, accusantium iste eligendi at possimus! Voluptatem quaerat consectetur,
-              alias debitis blanditiis impedit in provident corrupti a vitae tempore ipsam eaque,
-              voluptas cum delectus accusamus rerum odio! Qui amet accusantium quos maiores
-              incidunt.
+              onClick={() => handleOpen(8)}
+            >
+              One handyman will come for smaller to mid-sized jobs. Larger or
+              more complex jobs may require additional handymen to complete the
+              work.
             </AccordionItem>
             <AccordionItem
-              title='How do you handle cleanup?'
+              title="What forms of payment do you accept?"
               isOpen={open === 10}
-              onClick={() => handleOpen(10)}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iure.
+              onClick={() => handleOpen(10)}
+            >
+              We accept:
+              <br />
+              • Cheques
+              <br />
+              • e-Transfer
+              <br />
+              • Cash
+              <br />
+              <br />
+              We do not accept:
+              <br />
+              • MasterCard
+              <br />
+              • Visa
+              <br />• American Express
+            </AccordionItem>
+            <AccordionItem
+              title="Any other questions?"
+              isOpen={open === 12}
+              onClick={() => handleOpen(12)}
+            >
+              Contact us!
             </AccordionItem>
           </div>
         </div>
